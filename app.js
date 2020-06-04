54d9918d514ee17aa150445f43fb6c1264604f37
Yenisbel Valle <yenisbelv@gmail.com>
2020-06-04 10:06:05 -0700

    Add first commit initialize web front

---
+++ app.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const path = require('path');
+
+const handlebars = require('express-handlebars');
+
+app.engine('.hbs', handlebars({ extname: '.hbs' , defaultLayout: false }));
+
+app.set("PORT", PORT);
+
+app.use(express.static(path.join(__dirname, 'assets')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', '.hbs');
+
+app.get('/', function (req, res) {
+    
+    res.render("index", {title:"Jobscrapper"})
+});
+
+app.listen(app.get('PORT'), function () {
+    console.log('Express started on http://localhost:' +
+        app.get('PORT') + '; press Ctrl-C to terminate.');
+});
